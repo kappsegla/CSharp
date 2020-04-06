ed4e98e303dcd8b01e14fa23bb5d8ec378e9d66e
Martin Blomberg <martin.blomberg@outlook.com>
2020-04-06 08:57:36 +0200

    Program.cs updated

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -1,34 +1,23 @@
 ﻿using System;
-using System.Collections.Generic;
 
 namespace ConsoleApp
 {
     internal class Program
     {
-        private const int Age = 1;
-        private static bool canDo = false;
-
-        private string Name { get; set; }
-
-        // private static void Main(string[] args)
-        // {
-        //     Console.WriteLine("Hello World!");
-        //     var list = new List<int>();
-        //     
-        //     var p = new Program();
-        // }
-        //
-
-        static int ParseInt(string s)
+        static void Main(string[] args)
         {
-            // int i;
-            // bool b = Int32.TryParse(s, out i);
-            // if (b)
-            //     return i;
-            // throw new Exception("Couldn't parse string");
-            //
-            //Or
-            return Convert.ToInt32(s);
+            Console.WriteLine("Hello World!");
+            
+            
+            
+            
         }
+        
+        
+        
+        
+        
+        
+        
     }
 }
