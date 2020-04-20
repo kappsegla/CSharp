000803e3ec7e51e80d48bd61b031bb1b1bbdea15
Martin Blomberg <martin.blomberg@outlook.com>
2020-04-20 11:55:50 +0200

    Generics

---
+++ ConsoleApp/Generics/Storage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Generics
+{
+    class Storage<T>
+    {
+
+        private T _number;
+
+        public void Save(T number)
+        {
+            _number = number;
+        }
+
+        public T Get()
+        {
+            return _number;
+        }
+    }
+}
