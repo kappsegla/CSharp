f875578c61883b355f1f3b24ffa611eee843fe13
Martin Blomberg <martin.blomberg@outlook.com>
2020-04-20 13:20:39 +0200

    Exercise3 Hints

---
+++ ConsoleApp/Exercise3/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using ConsoleApp.Shapes;
+
+namespace ConsoleApp.Exercise3
+{
+    public class Program
+    {
+        // private static void Main(string[] args)
+        // {
+        //
+        //     DisplayMenu();
+        //
+        //
+        //
+        //
+        // }
+
+        public Shape CreateShape(ShapeType type){
+
+            switch (type)
+            {
+                case ShapeType.Ellipse:
+                    return new Ellipse();
+                case ShapeType.Rectangle:
+                    return new Rectangle();
+                default:
+                    return null;
+            }
+        }
+
+    }
+
+    public class Ellipse : Shape
+    {
+    }
+
+    public class Rectangle : Shape
+    {
+        
+    }
+
+    public enum ShapeType
+    {
+        Ellipse,
+        Rectangle
+    }
+    
+}
