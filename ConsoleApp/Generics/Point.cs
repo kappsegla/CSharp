3950d666117138998490398759aec013186ed17f
Martin Blomberg <martin.blomberg@outlook.com>
2020-04-27 10:26:00 +0200

    Added properties and DistanceTo method

--- ConsoleApp/Generics/Point.cs
+++ ConsoleApp/Generics/Point.cs
@@ -2,5 +2,15 @@
 
 public class Point
 {
-	
+	public double X { get; set; }
+	public double Y { get; set; }
+
+	public double DistanceTo(Point other)
+	{
+		var s1 = this.X - other.X;
+		var s2 = this.Y - other.Y;
+
+		var length = Math.Sqrt(s1 * s1 + s2 * s2);
+		return length;
+	}
 }
