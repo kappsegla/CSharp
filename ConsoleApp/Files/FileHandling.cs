e351666e077dba06d55c8893917701d694924f16
Martin Blomberg <martin.blomberg@outlook.com>
2020-05-10 11:04:07 +0200

    FileHandling

---
+++ ConsoleApp/Files/FileHandling.cs
@@ -0,0 +1,30 @@
+﻿using System.IO;
+using System.Text;
+
+namespace ConsoleApp.Files
+{
+    public class FileHandling
+    {
+        public void Run()
+        {
+            
+            //Directory
+             
+            //File
+            
+            var fs = new FileStream("",FileMode.Open);
+
+
+            //Text files
+
+            //Binary file
+
+            //File streams
+
+
+
+        }
+        
+        
+    }
+}
