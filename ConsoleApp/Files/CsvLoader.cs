3c336b11c7f7811f032df700fa969d9702eee050
Martin Blomberg <martin.blomberg@outlook.com>
2020-05-11 12:45:59 +0200

    Csv

---
+++ ConsoleApp/Files/CsvLoader.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections.Generic;
+using System.Text;
+
+namespace ConsoleApp.Files
+{
+    public class CsvLoader
+    {
+        public void Run()
+        {
+            var persons = new List<Person>()
+            {
+                new Person() {Name = "Martin", Age = 42, City = "Kalmar"},
+                new Person() {Name = "Anna", Age = 30, City = "Göteborg"}
+            };
+            
+            
+
+
+
+        }
+
+
+        public void LoadFromCsvFile()
+        {
+            
+        }
+        
+        public void SaveToCsvFile()
+        {
+            
+        }
+        
+    }
+}
---
+++ ConsoleApp/Files/Person.cs
@@ -0,0 +1,14 @@
+﻿namespace ConsoleApp.Files
+{
+    public class Person
+    {
+        public string Name { get; set; }
+        public int Age { get; set; }
+        public string City { get; set; }
+
+        public override string ToString()
+        {
+            return $"Name : {Name} Age : {Age} City : {City}";
+        }
+    }
+}
