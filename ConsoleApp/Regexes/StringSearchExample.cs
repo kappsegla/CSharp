25b770c95cec711491007e571b75b7e38908d768
Martin Blomberg <martin.blomberg@outlook.com>
2020-05-18 09:57:10 +0200

    Contains

--- ConsoleApp/Regexes/StringSearchExample.cs
+++ ConsoleApp/Regexes/StringSearchExample.cs
@@ -1,4 +1,8 @@
-﻿namespace ConsoleApp.Regexes
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace ConsoleApp.Regexes
 {
     public class StringSearchExample
     {
@@ -6,12 +10,33 @@
         {
             var text = "This is a string";
             
+            bool contains = text.Contains("is");
             
+            // Console.WriteLine(contains);
+            // Console.WriteLine(text.IndexOf("is"));
+
+            var products = new List<Product>()
+            {
+                new Product(){Name = "Apple"},
+                new Product(){Name = "Banana"},
+                new Product(){Name = "Apple Juice"},
+                new Product(){Name = "Apple Pie"}
+            };
+
+            var searchTerm = "apple";
             
-            
+            foreach (var product in products)
+            {
+                if( product.Name.Contains(searchTerm,StringComparison.CurrentCultureIgnoreCase) )
+                    Console.WriteLine(product.Name);
+            }
 
+            int count  = products.Where(p => p.Name.Contains(searchTerm)).Count();
+            }
+    }
 
-
-        }
+    public class Product
+    {
+        public string Name { get; set; }
     }
 }
